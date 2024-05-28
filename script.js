8a66c3f752b0340a015cd869f7160ab683b21fdf
nida_mumtaz <148453218+nida-shaikh@users.noreply.github.com>
2024-05-28 00:55:24 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function makeBold() {
+    var text = document.getElementById('text');
+    text.classList.toggle('bold');
+}
+
+
+function makeitalic(){
+    text.classList.toggle('italic');
+
+}
+function makeunderline(){
+    text.classList.toggle('underline');
+
+}
+// function changeTextColor() {
+//     var text = document.getElementById('text');
+//     var color = document.getElementById('colorPicker').value;
+//     text.style.color = color;
+// }
+// function textcoloring(){
+//     text.style.color = textcolor.value;
+// }
+// function changeTextColor() {
+
+//     var color = document.getElementById('colorPicker').value;
+//     text.style.color = color;
+// }
+
+
+function right(){
+    text.style.textAlign ='right';
+}
+
